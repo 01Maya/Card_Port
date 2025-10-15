567724acd02059eb0d8b8a3c21b21f9ba4467587
Mansi_Patel <patelmaya9812@gmail.com>
2025-10-15 21:53:44 +0530

    Update draggable-card-demo-2.tsx

--- components/draggable-card-demo-2.tsx
+++ components/draggable-card-demo-2.tsx
@@ -35,7 +35,7 @@ export default function DraggableCardDemo() {
       title: "Olea-Skin Care",
       image: "/Olea Skin Care.jpeg",
        className: "absolute top-6 left-[51%] rotate-[-5deg]",
-      link: "https://olea-skincare.vercel.app/",
+      link: "https://https://olea-skincare-landing-page.vercel.app/",
     },
     {
       title: "Bakery",
